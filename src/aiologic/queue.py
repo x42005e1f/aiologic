a9c9d17e5eaffd316fbd87350a3475c0c5101621
Ilya Egorov <0x42005e1f@gmail.com>
2024-10-19 16:58:31 +0400

    perf!: remove unnecessary SimpleQueue features

--- src/aiologic/queue.py
+++ src/aiologic/queue.py
@@ -37,50 +37,28 @@ class QueueFull(Exception):
 class SimpleQueue:
     __slots__ = (
         "__weakref__",
-        "__get_lock",
-        "__put_lock",
+        "__sem",
         "_data",
-        "maxsize",
     )
 
     @staticmethod
-    def __new__(cls, items=MISSING, /, maxsize=None):
-        if maxsize is None:
-            if isinstance(items, int):
-                items, maxsize = MISSING, items
-            else:
-                maxsize = 0
-
+    def __new__(cls, items=MISSING, /):
         self = super(SimpleQueue, cls).__new__(cls)
 
-        self._data = data = self._make(items)
-
-        self.__get_lock = Semaphore(len(data))
-
-        if maxsize > 0:
-            self.__put_lock = Semaphore(maxsize - len(data))
+        if items is not MISSING:
+            self._data = data = deque(items)
         else:
-            self.__put_lock = None
+            self._data = data = deque()
 
-        self.maxsize = maxsize
+        self.__sem = Semaphore(len(data))
 
         return self
 
     def __getnewargs__(self, /):
-        if (maxsize := self.maxsize) != 0:
-            args = (list(self._data), maxsize)
-        else:
-            args = (list(self._data),)
-
-        return args
+        return (list(self._data),)
 
     def __repr__(self, /):
-        if (maxsize := self.maxsize) != 0:
-            args_repr = f"{repr(list(self._data))}, maxsize={maxsize!r}"
-        else:
-            args_repr = repr(list(self._data))
-
-        return f"{self.__class__.__name__}({args_repr})"
+        return f"SimpleQueue({list(self._data)!r})"
 
     def __bool__(self, /):
         return bool(self._data)
@@ -88,99 +66,51 @@ class SimpleQueue:
     def __len__(self, /):
         return len(self._data)
 
+    def put(self, /, item):
+        self._data.append(item)
+        self.__sem.release()
+
     async def async_put(self, /, item, *, blocking=True):
-        if (put_lock := self.__put_lock) is not None:
-            success = await put_lock.async_acquire(blocking=blocking)
+        if blocking:
+            await checkpoint()
 
-            if not success:
-                raise QueueFull
-
-        self._put(item)
-
-        if (get_lock := self.__get_lock) is not None:
-            get_lock.async_release()
+        self._data.append(item)
+        self.__sem.async_release()
 
     def green_put(self, /, item, *, blocking=True, timeout=None):
-        if (put_lock := self.__put_lock) is not None:
-            success = put_lock.green_acquire(
-                blocking=blocking,
-                timeout=timeout,
-            )
+        if blocking:
+            green_checkpoint()
 
-            if not success:
-                raise QueueFull
-
-        self._put(item)
-
-        if (get_lock := self.__get_lock) is not None:
-            get_lock.green_release()
+        self._data.append(item)
+        self.__sem.green_release()
 
     async def async_get(self, /, *, blocking=True):
-        if (get_lock := self.__get_lock) is not None:
-            success = await get_lock.async_acquire(blocking=blocking)
+        success = await self.__sem.async_acquire(blocking=blocking)
 
-            if not success:
-                raise QueueEmpty
+        if not success:
+            raise QueueEmpty
 
-        item = self._get()
-
-        if (put_lock := self.__put_lock) is not None:
-            put_lock.async_release()
-
-        return item
+        return self._data.popleft()
 
     def green_get(self, /, *, blocking=True, timeout=None):
-        if (get_lock := self.__get_lock) is not None:
-            success = get_lock.green_acquire(
-                blocking=blocking,
-                timeout=timeout,
-            )
+        success = self.__sem.green_acquire(blocking=blocking, timeout=timeout)
 
-            if not success:
-                raise QueueEmpty
+        if not success:
+            raise QueueEmpty
 
-        item = self._get()
-
-        if (put_lock := self.__put_lock) is not None:
-            put_lock.green_release()
-
-        return item
-
-    def _make(self, /, items=MISSING):
-        if items is not MISSING:
-            data = deque(items)
-        else:
-            data = deque()
-
-        return data
-
-    def _put(self, /, item):
-        self._data.append(item)
-
-    def _get(self, /):
         return self._data.popleft()
 
     @property
     def waiting(self, /):
-        return abs(self.putting - self.getting)
+        return self.__sem.waiting
 
     @property
     def putting(self, /):
-        if (put_lock := self.__put_lock) is not None:
-            value = put_lock.waiting
-        else:
-            value = 0
-
-        return value
+        return 0
 
     @property
     def getting(self, /):
-        if (get_lock := self.__get_lock) is not None:
-            value = get_lock.waiting
-        else:
-            value = 0
-
-        return value
+        return self.__sem.waiting
 
 
 class Queue:
