1bcec8dc0d99c5e58df366cff8fcc7de474b75ae
Ilya Egorov <0x42005e1f@gmail.com>
2025-01-18 11:47:03 +0400

    fix: add old source tree compatibility

--- src/aiologic/__init__.py
+++ src/aiologic/__init__.py
@@ -42,6 +42,19 @@ from .semaphore import (
     Semaphore as Semaphore,
 )
 
+# add aiologic.locks subpackage for backward compatibility with 0.12.0
+__modules = __import__("sys").modules
+__modules[f"{__name__}.locks"] = __modules[f"{__name__}"]
+__modules[f"{__name__}.locks.barrier"] = __modules[f"{__name__}.barrier"]
+__modules[f"{__name__}.locks.condition"] = __modules[f"{__name__}.condition"]
+__modules[f"{__name__}.locks.event"] = __modules[f"{__name__}.event"]
+__modules[f"{__name__}.locks.guard"] = __modules[f"{__name__}.guard"]
+__modules[f"{__name__}.locks.limiter"] = __modules[f"{__name__}.limiter"]
+__modules[f"{__name__}.locks.lock"] = __modules[f"{__name__}.lock"]
+__modules[f"{__name__}.locks.semaphore"] = __modules[f"{__name__}.semaphore"]
+
+del __modules
+
 # modify __module__ for shorter repr() and better pickle support
 for __value in list(globals().values()):
     if getattr(__value, "__module__", "").startswith(f"{__name__}."):
