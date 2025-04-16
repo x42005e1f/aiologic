543de7ec8df217a73b5cfa9a00bc86329c5b1b8f
Ilya Egorov <0x42005e1f@gmail.com>
2025-04-17 01:45:39 +0400

    refactor!: rewrite aiologic.lowlevel._libraries

--- src/aiologic/lowlevel/_libraries.py
+++ src/aiologic/lowlevel/_libraries.py
@@ -3,13 +3,20 @@
 # SPDX-FileCopyrightText: 2024 Ilya Egorov <0x42005e1f@gmail.com>
 # SPDX-License-Identifier: ISC
 
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Final, Literal, overload
+
 from sniffio import (
-    AsyncLibraryNotFoundError,
-    thread_local as current_async_library_tlocal,
+    AsyncLibraryNotFoundError as AsyncLibraryNotFoundError,
+    thread_local,
 )
 from wrapt import when_imported
 
 from ._threads import ThreadLocal
+
+if TYPE_CHECKING:
+    from types import ModuleType
 
 
 class GreenLibraryNotFoundError(RuntimeError):
@@ -17,63 +24,64 @@ class GreenLibraryNotFoundError(RuntimeError):
 
 
 class _NamedLocal(ThreadLocal):
-    name = None
+    name: str | None = None
 
 
-current_green_library_tlocal = _NamedLocal()
+current_green_library_tlocal: Final[_NamedLocal] = _NamedLocal()
+current_async_library_tlocal: Final = thread_local
 
 
-def _eventlet_running():
+def _eventlet_running() -> bool:
     return False
 
 
-def _gevent_running():
+def _gevent_running() -> bool:
     return False
 
 
-def _asyncio_running():
+def _asyncio_running() -> bool:
     return False
 
 
-def _curio_running():
+def _curio_running() -> bool:
     return False
 
 
 @when_imported("eventlet")
-def _eventlet_running_hook(_):
+def _eventlet_running_hook(_: ModuleType) -> None:
     global _eventlet_running
 
-    def _eventlet_running():
+    def _eventlet_running() -> bool:
         global _eventlet_running
 
         from eventlet.hubs import _threadlocal
 
-        def _eventlet_running():
+        def _eventlet_running() -> bool:
             return getattr(_threadlocal, "hub", None) is not None
 
         return _eventlet_running()
 
 
 @when_imported("gevent")
-def _gevent_running_hook(_):
+def _gevent_running_hook(_: ModuleType) -> None:
     global _gevent_running
 
-    def _gevent_running():
+    def _gevent_running() -> bool:
         global _gevent_running
 
         from gevent._hub_local import get_hub_if_exists
 
-        def _gevent_running():
+        def _gevent_running() -> bool:
             return get_hub_if_exists() is not None
 
         return _gevent_running()
 
 
 @when_imported("asyncio")
-def _asyncio_running_hook(_):
+def _asyncio_running_hook(_: ModuleType) -> None:
     global _asyncio_running
 
-    def _asyncio_running():
+    def _asyncio_running() -> bool:
         global _asyncio_running
 
         from asyncio import _get_running_loop
@@ -83,13 +91,13 @@ def _asyncio_running_hook(_):
 
         if version_info >= (3, 12) and is_builtin:
 
-            def _asyncio_running():
+            def _asyncio_running() -> bool:
                 return _get_running_loop() is not None
 
         else:
             from sniffio import current_async_library_cvar
 
-            def _asyncio_running():
+            def _asyncio_running() -> bool:
                 return (
                     current_async_library_cvar.get() == "asyncio"
                     or _get_running_loop() is not None
@@ -99,10 +107,10 @@ def _asyncio_running_hook(_):
 
 
 @when_imported("curio")
-def _curio_running_hook(_):
+def _curio_running_hook(_: ModuleType) -> None:
     global _curio_running
 
-    def _curio_running():
+    def _curio_running() -> bool:
         global _curio_running
 
         from curio.meta import curio_running as _curio_running
@@ -110,26 +118,39 @@ def _curio_running_hook(_):
         return _curio_running()
 
 
-def current_green_library(*, failsafe=False):
+@overload
+def current_green_library(*, failsafe: Literal[False] = False) -> str: ...
+@overload
+def current_green_library(*, failsafe: Literal[True]) -> str | None: ...
+def current_green_library(*, failsafe: bool = False) -> str | None:
     if (name := current_green_library_tlocal.name) is not None:
         return name
-    elif _gevent_running():
+
+    if _gevent_running():
         return "gevent"
-    elif _eventlet_running():
+
+    if _eventlet_running():
         return "eventlet"
-    else:
-        return "threading"
+
+    return "threading"
 
 
-def current_async_library(*, failsafe=False):
+@overload
+def current_async_library(*, failsafe: Literal[False] = False) -> str: ...
+@overload
+def current_async_library(*, failsafe: Literal[True]) -> str | None: ...
+def current_async_library(*, failsafe: bool = False) -> str | None:
     if (name := current_async_library_tlocal.name) is not None:
         return name
-    elif _curio_running():
+
+    if _curio_running():
         return "curio"
-    elif _asyncio_running():
+
+    if _asyncio_running():
         return "asyncio"
-    elif failsafe:
+
+    if failsafe:
         return None
-    else:
-        msg = "unknown async library, or not in async context"
-        raise AsyncLibraryNotFoundError(msg)
+
+    msg = "unknown async library, or not in async context"
+    raise AsyncLibraryNotFoundError(msg)
