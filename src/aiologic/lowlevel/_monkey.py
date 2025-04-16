fa3b8cbfc1f8f2be55d3e3438973516065a9c70f
Ilya Egorov <0x42005e1f@gmail.com>
2025-04-17 01:22:26 +0400

    refactor!: rewrite aiologic.lowlevel._monkey

--- src/aiologic/lowlevel/_monkey.py
+++ src/aiologic/lowlevel/_monkey.py
@@ -3,25 +3,27 @@
 # SPDX-FileCopyrightText: 2025 Ilya Egorov <0x42005e1f@gmail.com>
 # SPDX-License-Identifier: ISC
 
+from __future__ import annotations
+
 from importlib import import_module
-from types import SimpleNamespace
+from types import ModuleType
 
 from wrapt import when_imported
 
 
-def _eventlet_patched(module_name):
+def _eventlet_patched(module_name: str, /) -> bool:
     return False
 
 
-def _gevent_patched(module_name):
+def _gevent_patched(module_name: str, /) -> bool:
     return False
 
 
 @when_imported("eventlet.patcher")
-def _eventlet_patched_hook(_):
+def _eventlet_patched_hook(_: ModuleType) -> None:
     global _eventlet_patched
 
-    def _eventlet_patched(module_name):
+    def _eventlet_patched(module_name: str, /) -> bool:
         global _eventlet_patched
 
         from eventlet.patcher import already_patched
@@ -35,20 +37,17 @@ def _eventlet_patched_hook(_):
             "threading": "thread",
         }
 
-        def _eventlet_patched(module_name):
-            if module_name in mapping:
-                return mapping[module_name] in already_patched
-            else:
-                return module_name in already_patched
+        def _eventlet_patched(module_name: str, /) -> bool:
+            return mapping.get(module_name, module_name) in already_patched
 
         return _eventlet_patched(module_name)
 
 
 @when_imported("gevent.monkey")
-def _gevent_patched_hook(_):
+def _gevent_patched_hook(_: ModuleType) -> None:
     global _gevent_patched
 
-    def _gevent_patched(module_name):
+    def _gevent_patched(module_name: str, /) -> bool:
         global _gevent_patched
 
         from gevent.monkey import is_module_patched as _gevent_patched
@@ -56,20 +55,27 @@ def _gevent_patched_hook(_):
         return _gevent_patched(module_name)
 
 
-def _patched(module_name):
+def _patched(module_name: str, /) -> bool:
     return _eventlet_patched(module_name) or _gevent_patched(module_name)
 
 
-_import_python_original = import_module
-_import_eventlet_original = import_module
-_import_gevent_original = import_module
+def _import_python_original(module_name: str, /) -> ModuleType:
+    return import_module(module_name)
+
+
+def _import_eventlet_original(module_name: str, /) -> ModuleType:
+    return import_module(module_name)
+
+
+def _import_gevent_original(module_name: str, /) -> ModuleType:
+    return import_module(module_name)
 
 
 @when_imported("eventlet.patcher")
-def _import_eventlet_original_hook(_):
+def _import_eventlet_original_hook(_: ModuleType) -> None:
     global _import_eventlet_original
 
-    def _import_eventlet_original(module_name):
+    def _import_eventlet_original(module_name: str, /) -> ModuleType:
         global _import_eventlet_original
 
         from eventlet.patcher import original as _import_eventlet_original
@@ -78,18 +84,20 @@ def _import_eventlet_original_hook(_):
 
 
 @when_imported("gevent.monkey")
-def _import_gevent_original_hook(_):
+def _import_gevent_original_hook(_: ModuleType) -> None:
     global _import_gevent_original
 
-    def _import_gevent_original(module_name):
+    def _import_gevent_original(module_name: str, /) -> ModuleType:
         global _import_gevent_original
 
-        from gevent.monkey import saved
+        from gevent.monkey import get_original
 
-        def _import_gevent_original(module_name):
-            return SimpleNamespace(**{
-                **vars(import_module(module_name)),
-                **saved.get(module_name, {}),
-            })
+        def _import_gevent_original(module_name: str, /) -> ModuleType:
+            names = dir(import_module(module_name))
+
+            module = ModuleType(module_name)
+            vars(module).update(zip(names, get_original(module_name, names)))
+
+            return module
 
         return _import_gevent_original(module_name)
