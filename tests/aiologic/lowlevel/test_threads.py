e162edbc6196c9de6998c55ca8e105883a433f55
Ilya Egorov <0x42005e1f@gmail.com>
2025-06-15 16:29:26 +0400

    test: add more specific tests for threads

--- tests/aiologic/lowlevel/test_threads.py
+++ tests/aiologic/lowlevel/test_threads.py
@@ -7,6 +7,8 @@ import weakref
 
 from concurrent.futures import ThreadPoolExecutor
 
+import pytest
+
 import aiologic
 
 
@@ -22,6 +24,44 @@ def test_current_thread():
     assert thread1 is not thread2
     assert thread1 is not thread3
     assert thread2 is not thread3
+
+
+def _test_current_thread_on(threading):
+    thread_obj = None
+    thread_ident = None
+
+    def run():
+        nonlocal thread_obj
+        nonlocal thread_ident
+
+        thread_obj = aiologic.lowlevel.current_thread()
+        thread_ident = aiologic.lowlevel.current_thread_ident()
+
+    thread = threading.Thread(target=run)
+    thread.start()
+    thread.join()
+
+    assert thread_obj is thread
+    assert thread_ident == thread.ident
+
+
+def test_current_thread_on_threading():
+    threading = pytest.importorskip("threading")
+
+    _test_current_thread_on(threading)
+
+
+def test_current_thread_on_eventlet():
+    try:  # see python-trio/trio#3015
+        import trio  # noqa: F401
+    except ImportError:
+        pass
+
+    patcher = pytest.importorskip("eventlet.patcher")
+
+    threading = patcher.original("threading")
+
+    _test_current_thread_on(threading)
 
 
 def test_current_thread_keying():
